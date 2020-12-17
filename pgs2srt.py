68f482873466b82acd37c10c9cecf766449018d6
Pim van der Loos <pimvanderloos@gmail.com>
2020-12-17 12:04:43 +0100

    Use argparse for cli
    - Using argparse for CLI interaction (i.e. specifying input/output files).
    - Allow specifying language and OEM mode from CLI.

--- pgs2srt.py
+++ pgs2srt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
-import os
+import argparse
 import pytesseract
 import re
 from PIL import Image, ImageOps
@@ -11,15 +10,23 @@ from datetime import datetime, timedelta
 from pgsreader import PGSReader
 from imagemaker import make_image
 
-# Filepath should be first argument
-file = sys.argv[1]
-if len(file) == 0:
-    sys.exit()
 
-# Unascape escaped spaces
-file = file.replace("\\ ", " ")
+parser = argparse.ArgumentParser(description='Convert PGS subtitles to SubRip format.')
 
-print("Parsing: "+file)
+parser.add_argument('input', type=str, help="The input file (a .sup file).")
+parser.add_argument('--output', type=str, help="The output file (a .srt file).")
+parser.add_argument('--oem', type=int, help="The OCR Engine Mode to use (Default: 1).", default=1)
+parser.add_argument('--language', type=str, help="The language to use (Default: eng).", default='eng')
+
+args = parser.parse_args()
+
+assert args.input is not None
+assert 0 <= args.oem <= 4
+
+# Unescape escaped spaces
+file = args.input.replace("\\ ", " ")
+
+print("Parsing: {}".format(file))
 
 # Load a PGS/SUP file.
 pgs = PGSReader(file)
@@ -30,28 +37,28 @@ i = 0
 # Complete subtitle track index
 si = 0
 
-tesseract_lang = "eng+swe"
-tesseract_config = "-c tessedit_char_blacklist=[] --psm 6"
+tesseract_lang = args.language
+tesseract_config = "-c tessedit_char_blacklist=[] --psm 6 --oem {}".format(args.oem)
+
+# If an output file for the subrip output is provided, use that.
+# Otherwise remove the ".sup" extension from the input and append
+# ".srt".
+output_file = args.output if args.output is not None else (args.input.replace('.sup', '') + '.srt')
 
 # SubRip output
 output = ""
 
-
 # Iterate the pgs generator
 for ds in pgs.iter_displaysets():
-
     try:
-
         # If set has image, parse the image
         if ds.has_image:
-
             # Get Palette Display Segment
             pds = ds.pds[0]
             # Get Object Display Segment
             ods = ds.ods[0]
 
             if pds and ods:
-
                 # Create and show the bitmap image and convert it to RGBA
                 src = make_image(ods, pds).convert('RGBA')
 
@@ -59,15 +66,14 @@ for ds in pgs.iter_displaysets():
                 img = Image.new("L", src.size, "BLACK")
                 # Paste the subtitle bitmap
                 img.paste(src, (0, 0), src)
-                # Invert images so the text is readable by teseract
+                # Invert images so the text is readable by Tesseract
                 img = ImageOps.invert(img)
 
                 # Parse the image with tesesract
-                text = pytesseract.image_to_string(
-                    img, lang=tesseract_lang, config=tesseract_config).strip()
+                text = pytesseract.image_to_string(img, lang=tesseract_lang, config=tesseract_config).strip()
 
                 # Replace "|" with "I"
-                # Works better than blacklisting "|" in tesseract,
+                # Works better than blacklisting "|" in Tesseract,
                 # which results in I becoming "!" "i" and "1"
                 text = re.sub(r'[|/\\]', 'I', text)
                 text = re.sub(r'[_]', 'L', text)
@@ -82,7 +88,6 @@ for ds in pgs.iter_displaysets():
                 start = start + timedelta(hours=-1)
 
         else:
-
             # Get Presentation Composition Segment
             pcs = ds.pcs[0]
 
@@ -91,23 +96,21 @@ for ds in pgs.iter_displaysets():
                 end = end + timedelta(hours=-1)
 
                 if isinstance(start, datetime) and isinstance(end, datetime) and len(text):
-                    si = si+1
-                    sub_output = str(si)+"\n"
+                    si = si + 1
+                    sub_output = str(si) + "\n"
                     sub_output += start.strftime("%H:%M:%S,%f")[0:12] + \
                         " --> " + end.strftime("%H:%M:%S,%f")[0:12]+"\n"
-                    sub_output += text+"\n\n"
+                    sub_output += text + "\n\n"
 
-                    print(sub_output[:-1])
                     output += sub_output
-
                     start = end = text = None
-
         i = i+1
-    except:
-        print("An exception occurred") 
 
-output_file = os.path.splitext(file)[0]+".srt"
-print("Saving to: "+output_file)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+print("Saving to: {}".format(output_file))
 f = open(output_file, "w")
 f.write(output)
 f.close()
